f942f191c99cc38b1e74f6b935d96ebd2816db62
kietdoanae <kiet.doanae@gmail.com>
2026-04-26 17:44:20 +0700

    feat(landing): add design tokens CSS variables

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { AuthProvider } from './context/AuthContext'
 import App from './App'
 import './index.css'
+import './styles/design-tokens.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
