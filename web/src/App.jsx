607d431f07a40fccfac81eb43ad2351fd36e3762
kietdoanae <kiet.doanae@gmail.com>
2026-04-27 08:02:45 +0700

    feat(routes): wrap /labs with StudentRoute, add /teach route

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -2,6 +2,9 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { ConfigProvider } from 'antd'
 import ProtectedRoute from './components/ProtectedRoute'
 import AdminRoute from './components/AdminRoute'
+import StudentRoute from './components/StudentRoute'
+import TeachRoute from './components/TeachRoute'
+import TeachPage from './pages/TeachPage'
 import LandingPage from './pages/LandingPage'
 import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
@@ -38,12 +41,13 @@ export default function App() {
           <Route path="/reset-password" element={<ResetPasswordPage />} />
           <Route path="/dashboard" element={<ProtectedRoute><DashboardPage /></ProtectedRoute>} />
           <Route path="/wiring" element={<ProtectedRoute><WiringPage /></ProtectedRoute>} />
-          <Route path="/labs" element={<ProtectedRoute><LabsListPage /></ProtectedRoute>} />
-          <Route path="/labs/:labId" element={<ProtectedRoute><LabOverviewPage /></ProtectedRoute>} />
-          <Route path="/labs/:labId/session/:sid" element={<ProtectedRoute><LabSessionPage /></ProtectedRoute>} />
-          <Route path="/labs/:labId/session/:sid/post" element={<ProtectedRoute><LabPostLabPage /></ProtectedRoute>} />
-          <Route path="/labs/:labId/session/:sid/report" element={<ProtectedRoute><LabReportPage /></ProtectedRoute>} />
-          <Route path="/my-reports" element={<ProtectedRoute><MyReportsPage /></ProtectedRoute>} />
+          <Route path="/labs" element={<StudentRoute><LabsListPage /></StudentRoute>} />
+          <Route path="/labs/:labId" element={<StudentRoute><LabOverviewPage /></StudentRoute>} />
+          <Route path="/labs/:labId/session/:sid" element={<StudentRoute><LabSessionPage /></StudentRoute>} />
+          <Route path="/labs/:labId/session/:sid/post" element={<StudentRoute><LabPostLabPage /></StudentRoute>} />
+          <Route path="/labs/:labId/session/:sid/report" element={<StudentRoute><LabReportPage /></StudentRoute>} />
+          <Route path="/my-reports" element={<StudentRoute><MyReportsPage /></StudentRoute>} />
+          <Route path="/teach" element={<TeachRoute><TeachPage /></TeachRoute>} />
           <Route path="/admin" element={<AdminRoute><AdminPage /></AdminRoute>} />
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
